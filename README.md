8d791d287dc3f5263c8e1c21a422151a7ca1cf2b
vasco <vastur@gmail.com>
2018-03-01 02:17:42 -0800

    Add code and bundler

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # three-spritetext
-A sprite based text component for Three.JS
+
+[![NPM](https://nodei.co/npm/three-forcegraph.png?compact=true)](https://nodei.co/npm/three-forcegraph/)
+
+A sprite based text component for [ThreeJS](https://threejs.org/). The text is drawn to canvas, converted into a [Texture](https://threejs.org/docs/#api/textures/Texture) and then used as a material on a [Sprite](https://threejs.org/docs/#api/objects/Sprite).
+Because a sprite is being used, the text will always face the camera, and have its orientation fixed relative to the camera.
+
+## Quick start
+
+```
+import SpriteText from 'three-spritetext';
+```
+or
+```
+var SpriteText = require('three-spritetext');
+```
+or even
+```
+<script src="//unpkg.com/three-spritetext"></script>
+```
+then
+```
+var myText = new SpriteText('My text');
+
+var myScene = new THREE.Scene();
+myScene.add(myText);
+```
+
+## API reference
+
+### Constructor
+
+<b>SpriteText</b> ([<i>text</i>, <i>textHeight</i>, <i>color</i>])
+
+### Properties
+
+| Property | Description | Default |
+| --- | --- | :--: |
+| <b>text</b> | The text to be displayed on the sprite. ||
+| <b>textHeight</b> | The height of the text. | 10 |
+| <b>color</b> | The color of the text. | `white` |
+| <b>fontFace</b> | The font type of the text. | `Arial` |
+| <b>fontSize</b> | The resolution of the text, in terms of vertical number of pixels. Lower values may cause text to look blurry. Higher values will yield sharper text, at the cost of performance. | `90` |
---
+++ src/index.js
@@ -0,0 +1,66 @@
+export default class extends THREE.Sprite {
+  constructor(text = '', textHeight = 10, color = 'rgba(255, 255, 255, 1)') {
+    super(new THREE.SpriteMaterial({ map: new THREE.Texture() }));
+
+    this._text = text;
+    this._textHeight = textHeight;
+    this._color = color;
+
+    this._fontFace = 'Arial';
+    this._fontSize = 90; // defines text resolution
+
+    this._canvas = document.createElement('canvas');
+    this._texture = this.material.map;
+    this._texture.minFilter = THREE.LinearFilter;
+
+    this._genCanvas();
+  }
+
+  get text() { return this._text; }
+  set text(text) { this._text = text; this._genCanvas(); }
+  get textHeight() { return this._textHeight; }
+  set textHeight(textHeight) { this._textHeight = textHeight; this._genCanvas(); }
+  get color() { return this._color; }
+  set color(color) { this._color = color; this._genCanvas(); }
+  get fontFace() { return this._fontFace; }
+  set fontFace(fontFace) { this._fontFace = fontFace; this._genCanvas(); }
+  get fontSize() { return this._fontSize; }
+  set fontSize(fontSize) { this._fontSize = fontSize; this._genCanvas(); }
+
+  _genCanvas() {
+    const canvas = this._canvas;
+    const ctx = canvas.getContext('2d');
+
+    const font = `normal ${this.fontSize}px ${this.fontFace}`;
+
+    ctx.font = font;
+    const textWidth = ctx.measureText(this.text).width;
+    canvas.width = textWidth;
+    canvas.height = this.fontSize;
+
+    ctx.font = font;
+    ctx.fillStyle = this.color;
+    ctx.textBaseline = 'bottom';
+    ctx.fillText(this.text, 0, canvas.height);
+
+    // Inject canvas into sprite
+    this._texture.image = canvas;
+    this._texture.needsUpdate = true;
+
+    this.scale.set(this.textHeight * canvas.width / canvas.height, this.textHeight);
+  }
+
+  clone() {
+    return new this.constructor(this.text, this.textHeight, this.color).copy(this);
+  }
+
+  copy(source) {
+    THREE.Sprite.prototype.copy.call(this, source);
+
+    this.color = source.color;
+    this.fontFace = source.fontFace;
+    this.fontSize = source.fontSize;
+
+    return this;
+  }
+}
